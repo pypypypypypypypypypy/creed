8eebe9cb637aa14ec7d92a67176835659de19e6b
abannition <maly4927@icloud.com>
2026-04-21 00:40:56 -0400

    feat: log Deafen actions to modlog

--- moderation/deafen.js
+++ moderation/deafen.js
@@ -2,6 +2,7 @@ const { EmbedBuilder, PermissionFlagsBits } = require('discord.js');
 const db = require('../db');
 const { default_prefix, color } = require('../config.json');
 const { warn, deny, approve } = require('../emojis.json');
+const { logModAction } = require('../utils/modlog');
 
 module.exports = {
   category: 'moderation',
@@ -50,5 +51,6 @@ module.exports = {
     } catch (err) {
       message.channel.send({ embeds: [new EmbedBuilder().setColor('#fe6464').setDescription(`${deny} ${message.author}: Failed to deafen member: ${err.message}`)] });
     }
+    logModAction(message.guild, { action: 'Deafen', user: member.user, moderator: message.author, reason: reason || 'No Reason Supplied' }).catch(() => {});
   }
 };
