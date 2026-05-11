11ce0974d3c1f1f97138e4d1f16e0542224f8747
abannition <maly4927@icloud.com>
2026-05-10 23:24:25 -0400

    feat: slash command support

--- fun/dare.js
+++ fun/dare.js
@@ -1,38 +1,30 @@
 const { EmbedBuilder } = require('discord.js');
-const db = require('../db');
 const { color } = require('../config.json');
 
 const dares = [
-  "Send a random emoji in chat",
-  "Type with your eyes closed for 1 message",
-  "Say something nice to someone here"
+  "Send a random emoji in chat right now.", "Type your next message with your eyes closed.",
+  "Say something genuinely nice about the last person who spoke.", "Set your status to something embarrassing for 10 minutes.",
+  "Send a voice message saying your favorite song badly.", "Share the last photo in your camera roll.",
+  "Compliment someone you normally wouldn't.", "Type everything in ALL CAPS for the next 5 minutes.",
+  "Do 10 push-ups and report back.", "Roast yourself in 2 sentences.",
 ];
 
 module.exports = {
   category: 'fun',
-  help: [
-    {
-        name: 'dare',
-        description: 'Get a random dare',
-        aliases: 'n/a',
-        parameters: 'n/a',
-        information: 'n/a',
-        usage: 'dare',
-        example: 'dare'
-    }
-],
+  name: 'dare',
+  help: [{ name: 'dare', description: 'Get a random dare', aliases: 'n/a', parameters: 'n/a', information: 'n/a', usage: 'dare', example: 'dare' }],
 
+  slashData: {
     name: 'dare',
+    description: 'Get a random dare',
+    dm_permission: true,
+    options: [],
+  },
+  runSlash: async (client, interaction) => {
+    await interaction.reply({ embeds: [new EmbedBuilder().setColor(color).setTitle('😈 Dare').setDescription(dares[Math.floor(Math.random() * dares.length)]).setTimestamp()] });
+  },
 
   run: async (client, message, args) => {
-    const dare = dares[Math.floor(Math.random() * dares.length)];
-
-    const embed = new EmbedBuilder()
-      .setColor(color)
-      .setTitle('😈 Dare')
-      .setDescription(dare)
-      .setTimestamp();
-
-    message.channel.send({ embeds: [embed] });
+    message.channel.send({ embeds: [new EmbedBuilder().setColor(color).setTitle('😈 Dare').setDescription(dares[Math.floor(Math.random() * dares.length)]).setTimestamp()] });
   }
 };
