526aef0254d6b7a78d907fd31d113304a20caf6b
abannition <maly4927@icloud.com>
2026-05-10 23:24:27 -0400

    feat: slash command support

--- utility/wikipedia.js
+++ utility/wikipedia.js
@@ -3,83 +3,56 @@ const { color } = require('../config.json');
 const { warn, loading: loadingEmoji } = require('../emojis.json');
 const fetch = require('node-fetch');
 
+async function searchWiki(query) {
+  const searchUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${encodeURIComponent(query)}&format=json&utf8=1`;
+  const searchData = await fetch(searchUrl).then(r => r.json());
+  if (!searchData.query.search.length) throw new Error('No results');
+  const title = searchData.query.search[0].title;
+  const summaryUrl = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts|pageimages&exintro=1&explaintext=1&titles=${encodeURIComponent(title)}&format=json&pithumbsize=400`;
+  const summaryData = await fetch(summaryUrl).then(r => r.json());
+  const page = Object.values(summaryData.query.pages)[0];
+  const description = (page.extract || 'No description.').slice(0, 1021);
+  const wikiUrl = `https://en.wikipedia.org/wiki/${encodeURIComponent(title.replace(/ /g, '_'))}`;
+  const embed = new EmbedBuilder().setColor(color)
+    .setAuthor({ name: 'Wikipedia', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/240px-Wikipedia-logo-v2.svg.png' })
+    .setTitle(title).setURL(wikiUrl).setDescription(description)
+    .setFooter({ text: 'Wikipedia' }).setTimestamp();
+  if (page.thumbnail?.source) embed.setThumbnail(page.thumbnail.source);
+  return embed;
+}
+
 module.exports = {
   category: 'utility',
-  help: [
-    {
-        name: 'wikipedia',
-        description: 'Search Wikipedia for an article',
-        aliases: 'wiki, wp',
-        parameters: '(query)',
-        information: 'n/a',
-        usage: 'wikipedia (query)',
-        example: 'wikipedia query'
-    }
-],
-
-    name: 'wikipedia',
+  name: 'wikipedia',
   aliases: ['wiki', 'wp'],
-  category: 'utility',
+  help: [{ name: 'wikipedia', description: 'Search Wikipedia', aliases: 'wiki, wp', parameters: '(query)', information: 'n/a', usage: 'wikipedia (query)', example: 'wikipedia Discord' }],
+
+  slashData: {
+    name: 'wikipedia',
+    description: 'Search Wikipedia for an article',
+    dm_permission: true,
+    options: [{ type: 3, name: 'query', description: 'What to search for', required: true }],
+  },
+  runSlash: async (client, interaction) => {
+    const query = interaction.options.getString('query');
+    await interaction.deferReply();
+    try {
+      await interaction.editReply({ embeds: [await searchWiki(query)] });
+    } catch {
+      await interaction.editReply({ content: `No Wikipedia results for **${query}**.` });
+    }
+  },
 
   run: async (client, message, args) => {
-    const helpEmbed = new EmbedBuilder()
-      .setAuthor({ name: message.author.username, iconURL: message.author.avatarURL({ forceStatic: false }) })
-      .setTitle('Command: wikipedia')
-      .setDescription('Search Wikipedia for a topic.')
-      .addFields(
-        { name: '**Aliases**', value: 'wiki, wp', inline: true },
-        { name: '**Parameters**', value: '[query]', inline: true },
-        { name: '**Information**', value: 'N/A', inline: true },
-        { name: '**Usage**', value: '```Syntax: ,wikipedia <query>\nExample: ,wikipedia Discord```' }
-      )
-      .setFooter({ text: 'Module: utility' })
-      .setTimestamp()
-      .setColor(color);
-
-    if (!args[0]) return message.channel.send({ embeds: [helpEmbed] });
-
-    const query = args.join(' ');
-    const loading = await message.channel.send({ embeds: [new EmbedBuilder().setColor(color).setDescription(`${loadingEmoji} ${message.author}: Searching Wikipedia...`)] });
-
+    if (!args[0]) return message.channel.send({ embeds: [new EmbedBuilder().setColor('#efa23a').setDescription(`${warn} ${message.author}: Usage: \`,wikipedia <query>\``)] });
+    const loading = await message.channel.send({ embeds: [new EmbedBuilder().setColor(color).setDescription(`${loadingEmoji} Searching...`)] });
     try {
-      const searchUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${encodeURIComponent(query)}&format=json&utf8=1`;
-      const searchRes = await fetch(searchUrl);
-      const searchData = await searchRes.json();
-
-      if (!searchData.query.search.length) {
-        await loading.delete().catch(() => {});
-        return message.channel.send({ embeds: [new EmbedBuilder().setColor('#fe6464').setDescription(`${warn} ${message.author}: No Wikipedia results found for **${query}**.`)] });
-      }
-
-      const title = searchData.query.search[0].title;
-      const summaryUrl = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts|pageimages&exintro=1&explaintext=1&titles=${encodeURIComponent(title)}&format=json&pithumbsize=400`;
-      const summaryRes = await fetch(summaryUrl);
-      const summaryData = await summaryRes.json();
-
-      const pages = summaryData.query.pages;
-      const page = pages[Object.keys(pages)[0]];
-      const extract = page.extract || 'No description available.';
-      const thumbnail = page.thumbnail?.source || null;
-      const wikiUrl = `https://en.wikipedia.org/wiki/${encodeURIComponent(title.replace(/ /g, '_'))}`;
-
-      const description = extract.length > 1024 ? extract.slice(0, 1021) + '...' : extract;
-
-      const embed = new EmbedBuilder()
-        .setColor(color)
-        .setAuthor({ name: 'Wikipedia', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/240px-Wikipedia-logo-v2.svg.png' })
-        .setTitle(title)
-        .setURL(wikiUrl)
-        .setDescription(description)
-        .setFooter({ text: `Wikipedia • Read more at the link above` })
-        .setTimestamp();
-
-      if (thumbnail) embed.setThumbnail(thumbnail);
-
+      const embed = await searchWiki(args.join(' '));
       await loading.delete().catch(() => {});
       message.channel.send({ embeds: [embed] });
-    } catch (e) {
+    } catch {
       await loading.delete().catch(() => {});
-      message.channel.send({ embeds: [new EmbedBuilder().setColor('#fe6464').setDescription(`${warn} ${message.author}: An error occurred while searching Wikipedia.`)] });
+      message.channel.send({ embeds: [new EmbedBuilder().setColor('#fe6464').setDescription(`${warn} ${message.author}: No results found.`)] });
     }
   }
 };
