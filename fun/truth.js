de7196972b544770694f3c235187398784c200e0
abannition <maly4927@icloud.com>
2026-05-10 23:24:24 -0400

    feat: slash command support

--- fun/truth.js
+++ fun/truth.js
@@ -1,38 +1,31 @@
 const { EmbedBuilder } = require('discord.js');
-const db = require('../db');
 const { color } = require('../config.json');
 
 const truths = [
-  "What is your biggest fear?",
-  "Have you ever lied to a friend?",
-  "What is your most embarrassing moment?"
+  "What is your biggest fear?", "Have you ever lied to a close friend?",
+  "What is your most embarrassing moment?", "What is one thing you've never told anyone?",
+  "What is the worst thing you've ever done?", "Have you ever cheated on a game or test?",
+  "What is something you are ashamed of?", "Who is your secret crush right now?",
+  "What is the most childish thing you still do?", "Have you ever ghosted someone?",
+  "What is a lie you've told recently?", "What do you secretly judge people for?",
 ];
 
 module.exports = {
   category: 'fun',
-  help: [
-    {
-        name: 'truth',
-        description: 'Get a random truth question',
-        aliases: 'n/a',
-        parameters: 'n/a',
-        information: 'n/a',
-        usage: 'truth',
-        example: 'truth'
-    }
-],
+  name: 'truth',
+  help: [{ name: 'truth', description: 'Get a random truth question', aliases: 'n/a', parameters: 'n/a', information: 'n/a', usage: 'truth', example: 'truth' }],
 
+  slashData: {
     name: 'truth',
+    description: 'Get a random truth question',
+    dm_permission: true,
+    options: [],
+  },
+  runSlash: async (client, interaction) => {
+    await interaction.reply({ embeds: [new EmbedBuilder().setColor(color).setTitle('💬 Truth').setDescription(truths[Math.floor(Math.random() * truths.length)]).setTimestamp()] });
+  },
 
   run: async (client, message, args) => {
-    const question = truths[Math.floor(Math.random() * truths.length)];
-
-    const embed = new EmbedBuilder()
-      .setColor(color)
-      .setTitle('💬 Truth')
-      .setDescription(question)
-      .setTimestamp();
-
-    message.channel.send({ embeds: [embed] });
+    message.channel.send({ embeds: [new EmbedBuilder().setColor(color).setTitle('💬 Truth').setDescription(truths[Math.floor(Math.random() * truths.length)]).setTimestamp()] });
   }
 };
