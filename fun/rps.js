a0c6d2007decdd6b00895a4f9e55ead8183fd803
abannition <maly4927@icloud.com>
2026-05-10 23:24:23 -0400

    feat: slash command support

--- fun/rps.js
+++ fun/rps.js
@@ -1,26 +1,60 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { color } = require('../config.json');
-const db = require('../db');
 
 const choices = { rock: '🪨', paper: '📄', scissors: '✂️' };
 const wins = { rock: 'scissors', paper: 'rock', scissors: 'paper' };
 
+async function playRPS(userId, replyFn, sendFn) {
+  const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder().setCustomId('rps_rock').setLabel('🪨 Rock').setStyle(ButtonStyle.Secondary),
+    new ButtonBuilder().setCustomId('rps_paper').setLabel('📄 Paper').setStyle(ButtonStyle.Secondary),
+    new ButtonBuilder().setCustomId('rps_scissors').setLabel('✂️ Scissors').setStyle(ButtonStyle.Secondary)
+  );
+  const msg = await replyFn(new EmbedBuilder().setColor(color).setTitle('Rock Paper Scissors').setDescription('Choose your move!'), row);
+  const collector = msg.createMessageComponentCollector({ filter: i => i.user.id === userId, time: 30000, max: 1 });
+  collector.on('collect', async i => {
+    const userChoice = i.customId.replace('rps_', '');
+    const botChoice = Object.keys(choices)[Math.floor(Math.random() * 3)];
+    let result;
+    if (userChoice === botChoice) result = "It's a **draw**!";
+    else if (wins[userChoice] === botChoice) result = 'You **win**! 🎉';
+    else result = 'You **lose**! 😢';
+    await i.update({ embeds: [new EmbedBuilder().setColor(color).setTitle('Rock Paper Scissors').setDescription(`You: ${choices[userChoice]} | Bot: ${choices[botChoice]}\n\n${result}`)], components: [] });
+  });
+  collector.on('end', (_, reason) => { if (reason === 'time') msg.edit({ components: [] }).catch(() => {}); });
+}
+
 module.exports = {
   category: 'fun',
-  help: [
-    {
-        name: 'rps',
-        description: 'Play rock paper scissors',
-        aliases: 'rockpaperscissors',
-        parameters: '(rock/paper/scissors)',
-        information: 'n/a',
-        usage: 'rps (rock/paper/scissors)',
-        example: 'rps rock/paper/scissors'
-    }
-],
-
-    name: 'rps',
+  name: 'rps',
   aliases: ['rockpaperscissors'],
+  help: [{ name: 'rps', description: 'Play rock paper scissors', aliases: 'rockpaperscissors', parameters: 'n/a', information: 'n/a', usage: 'rps', example: 'rps' }],
+
+  slashData: {
+    name: 'rps',
+    description: 'Play rock paper scissors',
+    dm_permission: true,
+    options: [],
+  },
+  runSlash: async (client, interaction) => {
+    await interaction.reply({ embeds: [new EmbedBuilder().setColor(color).setTitle('Rock Paper Scissors').setDescription('Choose your move!')], components: [new ActionRowBuilder().addComponents(
+      new ButtonBuilder().setCustomId('rps_rock').setLabel('🪨 Rock').setStyle(ButtonStyle.Secondary),
+      new ButtonBuilder().setCustomId('rps_paper').setLabel('📄 Paper').setStyle(ButtonStyle.Secondary),
+      new ButtonBuilder().setCustomId('rps_scissors').setLabel('✂️ Scissors').setStyle(ButtonStyle.Secondary)
+    )] });
+    const msg = await interaction.fetchReply();
+    const collector = msg.createMessageComponentCollector({ filter: i => i.user.id === interaction.user.id, time: 30000, max: 1 });
+    collector.on('collect', async i => {
+      const userChoice = i.customId.replace('rps_', '');
+      const botChoice = Object.keys(choices)[Math.floor(Math.random() * 3)];
+      let result;
+      if (userChoice === botChoice) result = "It's a **draw**!";
+      else if (wins[userChoice] === botChoice) result = 'You **win**! 🎉';
+      else result = 'You **lose**! 😢';
+      await i.update({ embeds: [new EmbedBuilder().setColor(color).setTitle('Rock Paper Scissors').setDescription(`You: ${choices[userChoice]} | Bot: ${choices[botChoice]}\n\n${result}`)], components: [] });
+    });
+    collector.on('end', (_, reason) => { if (reason === 'time') msg.edit({ components: [] }).catch(() => {}); });
+  },
 
   run: async (client, message, args) => {
     const row = new ActionRowBuilder().addComponents(
@@ -28,30 +62,17 @@ module.exports = {
       new ButtonBuilder().setCustomId('rps_paper').setLabel('📄 Paper').setStyle(ButtonStyle.Secondary),
       new ButtonBuilder().setCustomId('rps_scissors').setLabel('✂️ Scissors').setStyle(ButtonStyle.Secondary)
     );
-
-    const embed = new EmbedBuilder().setColor(color).setTitle('Rock Paper Scissors').setDescription('Choose your move!');
-    const msg = await message.channel.send({ embeds: [embed], components: [row] });
-
+    const msg = await message.channel.send({ embeds: [new EmbedBuilder().setColor(color).setTitle('Rock Paper Scissors').setDescription('Choose your move!')], components: [row] });
     const collector = msg.createMessageComponentCollector({ filter: i => i.user.id === message.author.id, time: 30000, max: 1 });
-
     collector.on('collect', async i => {
       const userChoice = i.customId.replace('rps_', '');
       const botChoice = Object.keys(choices)[Math.floor(Math.random() * 3)];
-
       let result;
       if (userChoice === botChoice) result = "It's a **draw**!";
       else if (wins[userChoice] === botChoice) result = 'You **win**! 🎉';
       else result = 'You **lose**! 😢';
-
-      const resultEmbed = new EmbedBuilder().setColor(color)
-        .setTitle('Rock Paper Scissors')
-        .setDescription(`You: ${choices[userChoice]} | Bot: ${choices[botChoice]}\n\n${result}`);
-
-      await i.update({ embeds: [resultEmbed], components: [] });
+      await i.update({ embeds: [new EmbedBuilder().setColor(color).setTitle('Rock Paper Scissors').setDescription(`You: ${choices[userChoice]} | Bot: ${choices[botChoice]}\n\n${result}`)], components: [] });
     });
-
-    collector.on('end', (_, reason) => {
-      if (reason === 'time') msg.edit({ components: [] }).catch(() => {});
-    });
+    collector.on('end', (_, reason) => { if (reason === 'time') msg.edit({ components: [] }).catch(() => {}); });
   }
 };
