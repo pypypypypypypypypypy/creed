06feda82924583a843f0e5a5ec1918ff4544704c
blesspython <maly4927@icloud.com>
2026-07-09 03:32:48 -0400

    Update hardcoded OWNER_ID to new owner

--- utils/owners.js
+++ utils/owners.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const OWNER_ID = '1503156125304881322';
+const OWNER_ID = '1517269354926833825';
 
 const ownerIds = new Set([OWNER_ID]);
 
