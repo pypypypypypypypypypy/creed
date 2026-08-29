ba8aea367b455eb5321de247a975cf7b982a2ba5
Drownwebsits <63968243-drownwebsits1@users.noreply.replit.com>
2026-08-29 11:46:56 +0000

    Update Discord owner ID

--- utils/owners.js
+++ utils/owners.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const OWNER_ID = '1525410659121107016';
+const OWNER_ID = '1541886962330374248';
 
 const ownerIds = new Set([OWNER_ID]);
 
